1dfe3306c2ca11efe489eb289654efd2aead4ad0
Glauber Costa Vila Verde <glauber.costa@linea.gov.br>
2016-03-07 15:14:07 -0300

    database configuration.

--- dri/settings.py
+++ dri/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'dri.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'dri',
+        'USER': 'driapi',
+        'PASSWORD': 'driapi',
+        'HOST': 'localhost',
+        'PORT': ''
     }
 }
 
