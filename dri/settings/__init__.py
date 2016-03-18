df9e8bef62b88c7a1044d1d79025c593575ef9b3
Glauber Costa Vila Verde <glauber.costa@linea.gov.br>
2016-03-18 14:23:03 -0300

    fixed import default development.py .

--- dri/settings/__init__.py
+++ dri/settings/__init__.py
@@ -1,3 +1,11 @@
 # Simple Package Organization for Environments
 # https://code.djangoproject.com/wiki/SplitSettings
-from dri.settings.development import *
+
+# Importa por default os settings de development caso nao haja variavel de ambiente setada
+# o valor dri.setting e default e esta setado em wsgi.py
+
+import os
+
+modules = os.environ.get('DJANGO_SETTINGS_MODULE', None)
+if modules == 'dri.settings':
+    from dri.settings.development import *
