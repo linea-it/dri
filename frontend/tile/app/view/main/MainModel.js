6cc1f645a724b5542ff5b12306620eb1c4ee3ad0
Glauber Costa Vila Verde <glauber.vila.verde@gmail.com>
2016-05-13 15:28:49 -0300

    The repository frontend-tiles has been moved to this repository.

---
+++ frontend/tile/app/view/main/MainModel.js
@@ -0,0 +1,14 @@
+/**
+ * This class is the view model for the Main view of the application.
+ */
+Ext.define('Tile.view.main.MainModel', {
+    extend: 'Ext.app.ViewModel',
+
+    alias: 'viewmodel.main',
+
+    data: {
+        name: 'Sky Viewer',
+        desPortalLogo: 'resources/des-portal-logo.png'
+    }
+
+});
