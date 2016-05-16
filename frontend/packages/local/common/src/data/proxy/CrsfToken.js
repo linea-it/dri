b26af72dd0e55a23608f0487aac356e10df0a9fd
Glauber Costa Vila Verde <glauber.vila.verde@gmail.com>
2016-05-16 17:27:12 -0300

    close #32 - POST methods integration ExtJS x Django

---
+++ frontend/packages/local/common/src/data/proxy/CrsfToken.js
@@ -0,0 +1,19 @@
+Ext.define('common.data.proxy.CrsfToken', {
+    singleton: true,
+
+    constructor: function () {
+        Ext.Ajax.on('beforerequest', function (conn, options) {
+            if (!(/^http:.*/.test(options.url) || /^https:.*/.test(options.url))) {
+                if (typeof(options.headers) == 'undefined') {
+                    options.headers = {
+                        'Accept': 'application/json',
+                        'X-CSRFToken': Ext.util.Cookies.get('csrftoken')
+                    };
+                } else {
+                    options.headers['Application'] = 'application/json';
+                    options.headers['X-CSRFToken'] = Ext.util.Cookies.get('csrftoken');
+                }
+            }
+        }, this);
+    }
+});
