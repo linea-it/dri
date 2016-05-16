b6189151bec717e791ebbc390933bf6471cbeedf
Glauber Costa Vila Verde <glauber.vila.verde@gmail.com>
2016-05-16 11:45:05 -0300

    Add directory builds frontend. Added apps directory with links pointing to the builds.

---
+++ frontend/build/temp/production/Tile/sass/config.rb
@@ -0,0 +1,2 @@
+Compass.add_project_configuration('../../../../../tile/sass/config.rb')
+cache_path = '/home/glauber/dri/frontend/build/.sass-cache'
---
+++ frontend/build/temp/production/Tile/slicer-temp/config.rb
@@ -0,0 +1,1 @@
+Compass.add_project_configuration('../../../../../tile/sass/config.rb')
