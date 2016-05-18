0ade3de6813340cae76ad6253ac00b6cc7fa8e1e
Alisson Lanot <alanot@gmail.com>
2016-05-18 17:57:49 -0300

    closed issue #39

--- api/product_classifier/models.py
+++ api/product_classifier/models.py
@@ -9,6 +9,9 @@ class ProductClass(models.Model):
 
     pcl_name = models.CharField(
         max_length=128, unique=True, verbose_name='Internal name, unique')
+    pcl_group = models.ForeignKey(
+        'product_classifier.ProductGroup',
+        on_delete=models.CASCADE)
     pcl_display_name = models.CharField(
         max_length=128, verbose_name='User-friendly display name')
     pcl_is_system = models.BooleanField(
@@ -16,3 +19,10 @@ class ProductClass(models.Model):
 
     def __str__(self):
         return self.pcl_display_name
+
+class ProductGroup(models.Model):
+
+    pgr_name = models.CharField(
+        max_length=128, unique=True, verbose_name='Internal name, unique')
+    pgr_display_name = models.CharField(
+        max_length=128, verbose_name='User-friendly display name')
