70ab5a627f6d9669fe9f9c33e9e03fd72d36c2ae
Glauber Costa Vila Verde <glauber.vila.verde@gmail.com>
2016-05-19 17:45:19 -0300

    #54 - Added missing files

--- frontend/tile/app/view/eyeballing/Aladin.js
+++ frontend/tile/app/view/eyeballing/Aladin.js
@@ -7,15 +7,27 @@ Ext.define('Tile.view.eyeballing.Aladin', {
         'aladin.Aladin'
     ],
 
-    initComponent: function () {
-        var me = this;
+    // Iniciar com footprint visivel
+    hideFootprint: false,
 
-        Ext.apply(this, {
-            hideFootprint: false,
-            tilesGridVisible: true
-        });
+    // Inicar com tile grid visivel
+    tilesGridVisible: true,
 
-        me.callParent(arguments);
-    }
+    // Inicar com nivel de zoom mais longe da tile.
+    initialFov: 4,
+
+    auxTools: [
+        {
+            xtype: 'button',
+            iconCls: 'x-fa fa-exclamation-triangle',
+            text: 'Flag',
+            tooltip: 'Flag/Unflag',
+            enableToggle: true,
+            toggleHandler: 'onFlagDataset',
+            bind: {
+                pressed: '{flagged.flg_flagged}'
+            }
+        }
+    ]
 
 });
