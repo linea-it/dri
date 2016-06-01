89ce8a9068224142caa30d4062e0b5191f29f872
Glauber Costa Vila Verde <glauber.vila.verde@gmail.com>
2016-06-01 13:45:30 -0300

    Added filter by group to viewset Product #35

--- api/product/views.py
+++ api/product/views.py
@@ -1,11 +1,24 @@
 import logging
 
+import django_filters
+from rest_framework import filters
 from rest_framework import viewsets
 from .models import Product
 from .serializers import ProductSerializer
 
 logger = logging.getLogger(__name__)
 
+
+class ProductFilter(django_filters.FilterSet):
+    group = django_filters.MethodFilter()
+
+    class Meta:
+        model = Product
+        fields = ['prd_name', 'prd_display_name', 'prd_class', 'group']
+
+    def filter_group(self, queryset, value):
+        # product -> product_class -> product_group
+        return queryset.filter(prd_class__pcl_group__pgr_name=str(value))
 
 # Create your views here.
 class ProductViewSet(viewsets.ModelViewSet):
@@ -18,6 +31,8 @@ class ProductViewSet(viewsets.ModelViewSet):
 
     search_fields = ('prd_name', 'prd_display_name', 'prd_class')
 
-    filter_fields = ('id', 'prd_name', 'prd_display_name', 'prd_class')
+    filter_backends = (filters.DjangoFilterBackend,)
+
+    filter_class = ProductFilter
 
     ordering_fields = ('id', 'prd_name', 'prd_display_name', 'prd_class')
