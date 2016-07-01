3c68444d573f4ee01801b44832a8acaaf9cf0f24
Alisson Lanot <alanot@gmail.com>
2016-07-01 15:43:31 -0300

    added model and dependency

--- api/interfaces/models.py
+++ api/interfaces/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Applications(models.Model):
+    app_name = models.CharField(
+        max_length=128, verbose_name='Internal Name')
+    app_url = models.CharField(
+        max_length=128, verbose_name='URL')
+    app_short_description = models.CharField(
+        max_length=128, verbose_name='Short Description')
+    app_long_description = models.CharField(
+        max_length=1024, verbose_name='Long Description')
+    app_icon = models.ImageField()
+    app_thumbnail = models.ImageField()
+
+    def __str__(self):
+        return self.app_name
