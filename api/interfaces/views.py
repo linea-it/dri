8fd1bc5be062cc8f95b9b50310817d0fdfa47376
Alisson Lanot <alanot@gmail.com>
2016-07-01 16:04:01 -0300

    added Application's viewset

--- api/interfaces/views.py
+++ api/interfaces/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Application 
+from .serializers import ApplicationSerializer
 
-# Create your views here.
+
+class ApplicationViewSet(viewsets.ModelViewSet):
+
+    queryset = Application.objects.all()
+
+    serializer_class = ApplicationSerializer
+
+    search_fields = ('app_name')
+
+    filter_fields = ('id', 'app_name')
+
+    ordering_fields = ('id', 'app_name')
