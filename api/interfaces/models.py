5e4660a4a7d93aecd89e25c975d649f6c6229082
Alisson Lanot <alanot@gmail.com>
2016-07-01 15:46:05 -0300

    updated url field for application model

--- api/interfaces/models.py
+++ api/interfaces/models.py
@@ -3,8 +3,8 @@ from django.db import models
 class Applications(models.Model):
     app_name = models.CharField(
         max_length=128, verbose_name='Internal Name')
-    app_url = models.CharField(
-        max_length=128, verbose_name='URL')
+    app_url = models.URLField(
+        verbose_name='URL')
     app_short_description = models.CharField(
         max_length=128, verbose_name='Short Description')
     app_long_description = models.CharField(
