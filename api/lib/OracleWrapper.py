a776ba1698fc4c5f3e218e2173d77e844050f6e8
Glauber Costa Vila Verde <glauber.vila.verde@gmail.com>
2016-07-04 10:20:49 -0300

    Query with ROWNUN in Oracle.

--- api/lib/OracleWrapper.py
+++ api/lib/OracleWrapper.py
@@ -10,62 +10,61 @@ class OracleWrapper(BaseWrapper):
         Return all columns in a table"
         """
         query = "SELECT * FROM %s WHERE ROWNUM <= 1;" % table
-        print(query)
         cursor = self.execute(query)
         columns = [col[0] for col in cursor.description]
 
         return columns
 
-    def query(self, table, schema=None, columns=None, filters=None, order_by=None, limit=None, offset=None, dict=True):
-
-        sql_columns = '*'
-        sql_from = ''
-        sql_where = ''
-        sql_sort = ''
-        sql_limit = ''
-        sql_count = None
-
-        if schema:
-            sql_from = '%s.%s' %(schema, table)
-        else:
-            sql_from = table
-
-        # Lista de colunas da tabela
-        tbl_columns = self.get_table_columns(sql_from)
-        tbl_columns = list()
-        if columns:
-            sql_columns = ', '.join(self.__check_columns(tbl_columns, columns))
-
-        # if filters:
-        #     sql_filter = self.parseFilter(filters, checkColumns=tbl_columns)
-
-        # if order_by:
-        #     sql_sort = self.parseSort(order_by, tbl_columns)
-
-        if limit:
-            sql_limit = self.wrapper.parseLimit(limit, offset, is_filtered)
-
-        sql = ("SELECT %s FROM %s %s %s %s;") % (sql_columns, sql_from, sql_where, sql_sort, sql_limit)
-
-        if limit:
-            sql_count = ("SELECT COUNT(*) as count FROM %s %s %s") % (sql_from, sql_where, sql_sort)
-
-        print("SQL: %s" % sql)
-        print("SQL Count: %s" % sql_count)
-
-        if dict:
-            rows = self.fetchall_dict(sql)
-        else:
-            rows = self.fetchall(sql)
-
-        if sql_count:
-            count = self.fetchall(sql_count)[0][0]
-            print(count)
-            print("Count: %s" % count)
-        else:
-            count = len(rows)
-
-        return rows, count
+    # def query(self, table, schema=None, columns=None, filters=None, order_by=None, limit=None, offset=None, dict=True):
+    #
+    #     sql_columns = '*'
+    #     sql_from = ''
+    #     sql_where = ''
+    #     sql_sort = ''
+    #     sql_limit = ''
+    #     sql_count = None
+    #
+    #     if schema:
+    #         sql_from = '%s.%s' %(schema, table)
+    #     else:
+    #         sql_from = table
+    #
+    #     # Lista de colunas da tabela
+    #     tbl_columns = self.get_table_columns(sql_from)
+    #     tbl_columns = list()
+    #     if columns:
+    #         sql_columns = ', '.join(self.__check_columns(tbl_columns, columns))
+    #
+    #     # if filters:
+    #     #     sql_filter = self.parseFilter(filters, checkColumns=tbl_columns)
+    #
+    #     # if order_by:
+    #     #     sql_sort = self.parseSort(order_by, tbl_columns)
+    #
+    #     # if limit:
+    #     #     sql_limit = self.wrapper.parseLimit(limit, offset, is_filtered)
+    #
+    #     sql = ("SELECT %s FROM %s %s %s %s;") % (sql_columns, sql_from, sql_where, sql_sort, sql_limit)
+    #
+    #     if limit:
+    #         sql_count = ("SELECT COUNT(*) as count FROM %s %s %s") % (sql_from, sql_where, sql_sort)
+    #
+    #     print("SQL: %s" % sql)
+    #     print("SQL Count: %s" % sql_count)
+    #
+    #     if dict:
+    #         rows = self.fetchall_dict(sql)
+    #     else:
+    #         rows = self.fetchall(sql)
+    #
+    #     if sql_count:
+    #         count = self.fetchall(sql_count)[0][0]
+    #         print(count)
+    #         print("Count: %s" % count)
+    #     else:
+    #         count = len(rows)
+    #
+    #     return rows, count
 
     def __check_columns(self, a, b):
         """
