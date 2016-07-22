61c99cc0bdc4e8f1edb3b73e1cb5418cb4eb118a
Glauber Costa Vila Verde <glauber.vila.verde@gmail.com>
2016-07-22 17:19:42 -0300

    CORS origin enable by default in production settings.

--- api/dri/settings/production.py
+++ api/dri/settings/production.py
@@ -3,7 +3,7 @@ from dri.settings.defaults import *
 from dri.settings.local_vars import *
 
 DEBUG = False
-CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 # CORS_ORIGIN_WHITELIST = (         # [CMP] to enable in setting/local.py
 #     # 'hostname.example.com'
