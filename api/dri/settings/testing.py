4d674dcc845479036274bf640b1bf3ecd1e2e94c
campisano <campisano@pavilion076>
2016-08-29 16:40:01 -0300

    Closed #198

--- api/dri/settings/testing.py
+++ api/dri/settings/testing.py
@@ -1,7 +1,9 @@
-# Load defaults and local_vars in order to then add/override with testing-only settings
+# Load defaults and local_vars in order to then add/override with development-only settings
 from dri.settings.defaults import *
 from dri.settings.local_vars import *
 
-DEBUG = True
+ALLOWED_HOSTS = '*'
+CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_ALLOW_ALL = True
-CORS_ALLOW_CREDENTIALS = False
+
+DEBUG = True
