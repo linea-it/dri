4adfa675810d3c39bfe4d47b44de9f7eca46b59a
Glauber Costa Vila Verde <glauber.vila.verde@gmail.com>
2016-09-15 14:40:39 -0300

    Add mark to recent releases, which were added to 2 days.

--- frontend/packages/local/common/src/model/Release.js
+++ frontend/packages/local/common/src/model/Release.js
@@ -10,6 +10,20 @@ Ext.define('common.model.Release', {
         {name:'rls_doc_url', type:'string'},
         {name:'rls_display_name', type:'string'},
         {name:'rls_default', type:'boolean'},
-        {name:'tiles_count', type:'int'}
+        {name:'tiles_count', type:'int'},
+        {
+            name: 'is_new',
+            type: 'boolean',
+            convert: function (value, record) {
+                var create_date = record.get('rls_date'),
+                    interval = -2,
+                    sysdate = Ext.Date.add(new Date(), Ext.Date.DAY, interval);
+
+                // console.log('create_date: %o', create_date);
+                // console.log('sysdate: %o', sysdate);
+
+                return teste = Ext.Date.between(create_date, sysdate, create_date);
+            }
+        }
     ]
 });
--- frontend/sky/app/view/home/Home.js
+++ frontend/sky/app/view/home/Home.js
@@ -17,7 +17,22 @@ Ext.define('Sky.view.home.Home', {
     controller: 'home',
 
     columns: [
-        {text: 'Name',  dataIndex: 'rls_display_name', flex: 1},
+        {
+            text: 'Name',
+            dataIndex: 'rls_display_name',
+            flex: 1,
+            renderer: function (value, cell, record) {
+                console.log(value);
+                console.log(record);
+                if (record.get('is_new')) {
+                    // return value + '    <spam style="color:#e67e22;">New</spam>';
+                    return '<spam style="color:#e67e22;">New</spam>    ' + value;
+
+                }
+                return value;
+            }
+        },
+        {text: 'New',  dataIndex: 'is_new'},
         {xtype: 'datecolumn', text: 'Date', dataIndex: 'rls_date', format:'Y-m-d', flex: 1},
         {text: 'Tiles', dataIndex: 'tiles_count', flex: 1}
     ],
