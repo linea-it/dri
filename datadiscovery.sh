60abaa8af1c7ba7cb3c3d2b435b811b47afc2dbd
campisano <campisano@pavilion076>
2016-09-19 14:21:45 -0300

    Datadiscovery script comment fix

--- datadiscovery.sh
+++ datadiscovery.sh
@@ -11,20 +11,20 @@ function error_exit {
 }
 
 echo
-echo "= Datadiscovey ="
+echo "= Data Discovery ="
 cd $DRI_HOME
 
 echo
 echo "= Setting up env ="
-source env/bin/activate || error_exit "Error, exit" 6
+source env/bin/activate || error_exit "Error, exit" 1
 
 echo
 echo "= Entering in the application folder ="
-cd api  || error_exit "Error, exit" 7
+cd api || error_exit "Error, exit" 2
 
 echo
 echo "= Running command ="
-python manage.py datadiscovery || error_exit "Error, exit" 8
+python manage.py datadiscovery || error_exit "Error, exit" 3
 
 echo
 echo "= Exiting ="
