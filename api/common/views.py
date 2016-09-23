2c1f8fd1ff2cababba703d3d42dcd2812a0b7286
Glauber Costa Vila Verde <glauber.vila.verde@gmail.com>
2016-09-23 11:27:03 -0300

    Closed #270 - Add function Contact us, Closed #252 - Create a standard footer for Apps.

--- api/common/views.py
+++ api/common/views.py
@@ -1,12 +1,19 @@
 # Create your views here.
 from requests import Response
+from smtplib import SMTPException
 
 from rest_framework.views import APIView
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 from rest_framework import viewsets
-from rest_framework.mixins import RetrieveModelMixin
+from rest_framework.response import Response
 from django.contrib.auth.models import User
 from .models import Filter
 from .serializers import FilterSerializer, UserSerializer
+from django.core.mail import send_mail
+from django.http import HttpResponse
+from django.conf import settings
+from rest_framework import status
 
 class FilterViewSet(viewsets.ModelViewSet):
     """
@@ -21,14 +28,6 @@ class FilterViewSet(viewsets.ModelViewSet):
 
     ordering_fields = ('lambda_min', 'lambda_max', 'lambda_mean',)
 
-
-# class CurrentUserViewSet(viewsets.ViewSet):
-#     # queryset = User.objects.all()
-#     # serializer_class = UserSerializer
-#     def get(self, request):
-#         print("------- TESTE -----------")
-#         serializer = UserSerializer(request.user)
-#         return Response(serializer.data)
 
 class LoggedUserViewSet(viewsets.ModelViewSet):
 
@@ -47,5 +46,48 @@ class LoggedUserViewSet(viewsets.ModelViewSet):
 
     def get_logged(self):
         return self.request.user
+
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def contact_us(request):
+    """
+        API Endpoint to send a email to helpdesk
+    Args:
+        request:
+
+    Returns:
+
+    """
+    if request.method == 'POST':
+
+        # Dados da Mensagem
+        name = request.data.get('name', None)
+        from_email = request.data.get('from', None)
+        subject = request.data.get('subject', None)
+        message = request.data.get('message', None)
+
+        # Dados Tecnicos
+        current_url = request.data.get('current_url', None)
+        current_user = request.data.get('current_user', None)
+
+        if name is not None and from_email is not None and subject is not None and message is not None:
+
+            to_email = settings.EMAIL_HELPDESK
+
+            try:
+                send_mail(
+                    subject,
+                    message,
+                    from_email,
+                    [to_email],
+                    fail_silently=False,
+                )
+
+                return Response({"message": "Message sent successfully!"})
+
+            except SMTPException as e:
+                return Response(e, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
