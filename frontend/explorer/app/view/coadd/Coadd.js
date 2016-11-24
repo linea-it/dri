82a915b05b58b5936ec1175a9db50373fc6a9c9c
Glauber Costa Vila Verde <glauber.vila.verde@gmail.com>
2016-11-24 17:45:44 -0200

    Created new app Explorer

---
+++ frontend/explorer/app/view/coadd/Coadd.js
@@ -0,0 +1,17 @@
+Ext.define('Explorer.view.coadd.Coadd', {
+    extend: 'Ext.panel.Panel',
+
+    xtype: 'coadd-objects',
+
+    requires: [
+    ],
+
+    // controller: 'home',
+
+    // viewModel: 'home',
+
+    layout: 'fit',
+
+    html: 'Teste'
+
+});
