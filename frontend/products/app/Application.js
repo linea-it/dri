e46c0a3f975a3d0695d5bd4de83d5cc29650c271
Glauber Costa Vila Verde <glauber.vila.verde@gmail.com>
2017-01-18 17:31:46 -0200

    Fixed missing username in header.

--- frontend/products/app/Application.js
+++ frontend/products/app/Application.js
@@ -27,7 +27,6 @@ Ext.define('Products.Application', {
             url: '/dri/api/logged/get_logged/?format=json',
             success: function (response) {
                 var data = JSON.parse(response.responseText);
-                window.sessionStorage.setItem('dri_username', data.username);
 
                 // Identificar o usuario no Google Analitics
                 ga('set', 'userId', data.id);
