c99fa19a2b93a4d9edf8270dea4eeb4c7f3c292f
campisano <campisano@pavilion076>
2017-03-16 14:57:55 -0300

    Test reverting last build changes to recover Target images
    Revert "change maglim to 26.0"
    
    This reverts commit 4a914e19d885ab86d731f17806074aebd0e4235e.

--- frontend/packages/local/visiomatic/src/Visiomatic.js
+++ frontend/packages/local/visiomatic/src/Visiomatic.js
@@ -130,7 +130,7 @@ Ext.define('visiomatic.Visiomatic', {
                 name: 'Y3A1',
                 attribution: 'Des Y3A1 COADD OBJECT SUMMARY',
                 color: 'blue',
-                maglim: 26.0,
+                maglim: 24.0,
                 service: 'ScienceServer',
                 regionType: 'box',
                 authenticate: 'csrftoken',
