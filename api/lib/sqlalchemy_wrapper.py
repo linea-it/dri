8c08ab54f9bab31ae5c3b84e453c5c9b5ad46ed3
lucasdpn <lucasdpnunes@gmail.com>
2017-05-09 16:20:35 -0300

    missing file - Using SqlAlchemy to manipulate Catalogs.

---
+++ api/lib/sqlalchemy_wrapper.py
@@ -0,0 +1,97 @@
+from sqlalchemy import create_engine, inspect, MetaData, func, Table
+from sqlalchemy.sql import select
+
+
+class DBOracle:
+    def __init__(self, db):
+        self.db = db
+
+    def get_string_connection(self):
+        url = ("oracle://%(username)s:%(password)s@(DESCRIPTION=(" +
+              "ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=%(host)s)(" +
+              "PORT=%(port)s)))(CONNECT_DATA=(SERVER=dedicated)(" +
+              "SERVICE_NAME=%(database)s)))") %\
+              {"username": self.db['USER'], 'password': self.db['PASSWORD'],
+               'host': self.db['HOST'], 'port': self.db['PORT'],
+               'database': self.db['DATABASE']}
+        return url
+
+    def get_engine(self):
+        return "oracle"
+
+
+class DBSqlite:
+    def __init__(self, db):
+        self.db = db
+
+    def get_string_connection(self):
+        url = "sqlite:///%s" % self.db['PATH_FILE']
+        return url
+
+    def get_engine(self):
+        return "sqlite3"
+
+
+# classe generica - nao ligada a este problema
+class DBBase:
+    available_engines = list(['sqlite3', 'oracle'])
+
+    def __init__(self, db_settings):
+        self.database = self.set_database(db_settings)
+        self.engine = create_engine(self.database.get_string_connection())
+        self.inspect = inspect(self.engine)
+
+        with self.engine.connect():
+            self.metadata = MetaData(self.engine)
+
+    def set_database(self, db_settings):
+        if db_settings['ENGINE'] not in self.available_engines:
+            raise Exception('Unavailable engine')
+
+        if db_settings['ENGINE'] == 'sqlite3':
+            return DBSqlite(db_settings)
+
+        if db_settings['ENGINE'] == 'oracle':
+            return DBOracle(db_settings)
+
+    def get_string_connection(self):
+        return self.database.get_string_connection()
+
+    def get_engine(self):
+        return self.database.get_engine()
+
+    def get_table_columns(self, table, schema=None):
+        return [value['name'] for value in self.inspect.get_columns(table, schema)]
+
+    def get_count(self, table, schema=None):
+        with self.engine.connect() as con:
+            table = Table(table, self.metadata,
+                          autoload=True, schema=schema)
+            stm = select([func.count()]).select_from(table)
+            result = con.execute(stm)
+            return result.fetchone()[0]
+
+    def table_exists(self, table, schema=None):
+        return self.engine.has_table(table, schema)
+
+    def get_table_obj(self, table, schema=None):
+        return Table(table, self.metadata, autoload=True, schema=schema)
+
+    def get_column_obj(self, table_obj, column_name):
+        return getattr(table_obj.c, column_name)
+
+    def fetchall_dict(self, stm, columns):
+        with self.engine.connect() as con:
+            result = con.execute(stm)
+            return [
+                dict(zip(columns, row))
+                for row in result.fetchall()
+                ]
+
+    def select_all(self, table, schema=None):
+        with self.engine.connect() as con:
+            table = Table(table, self.metadata,
+                          autoload=True, schema=schema)
+            stm = select([table]).select_from(table)
+            result = con.execute(stm)
+            return result.fetchall()
