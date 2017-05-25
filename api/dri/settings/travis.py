2e7988180348ef5a8ef1c92e898c55b7eab79958
Glauber Costa Vila Verde <glauber.vila.verde@gmail.com>
2017-05-25 11:12:19 -0300

    #452 - Configure TravisCi

---
+++ api/dri/settings/travis.py
@@ -0,0 +1,45 @@
+from dri.settings.defaults import *
+
+import os
+
+print("Using Settings Travis")
+
+BASE_PROJECT = os.environ.get("TRAVIS_BUILD_DIR")
+print("BaseProject %s" % BASE_PROJECT)
+
+ALLOWED_HOSTS = '*'
+CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_ALLOW_ALL = True
+
+DEBUG = True
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_PROJECT, 'db/dri.db'),
+    },
+    'catalog': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_PROJECT, 'db/catalog.db'),
+    }
+}
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(BASE_PROJECT, 'log/test.log'),
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
