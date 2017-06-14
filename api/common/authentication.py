b12dfeb0b32aea9cf68117d369136ccb032dd2c4
Glauber Costa Vila Verde <glauber.vila.verde@gmail.com>
2017-06-14 17:07:15 -0300

    Fix test erro with authenticate

--- api/common/authentication.py
+++ api/common/authentication.py
@@ -29,19 +29,19 @@ class NcsaBackend(object):
 
     def check_user(self, username, password):
 
-        db = settings.DATABASES.get('dessci')
-        host, name = db.get('NAME').split('/')
-        host, port = host.split(':')
-
-        url = ("oracle://%(username)s:%(password)s@(DESCRIPTION=(" +
-               "ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=%(host)s)(" +
-               "PORT=%(port)s)))(CONNECT_DATA=(SERVER=dedicated)(" +
-               "SERVICE_NAME=%(database)s)))") % \
-              {"username": username, 'password': password,
-               'host': host, 'port': port,
-               'database': name}
-
         try:
+            db = settings.DATABASES.get('dessci')
+            host, name = db.get('NAME').split('/')
+            host, port = host.split(':')
+
+            url = ("oracle://%(username)s:%(password)s@(DESCRIPTION=(" +
+                   "ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=%(host)s)(" +
+                   "PORT=%(port)s)))(CONNECT_DATA=(SERVER=dedicated)(" +
+                   "SERVICE_NAME=%(database)s)))") % \
+                  {"username": username, 'password': password,
+                   'host': host, 'port': port,
+                   'database': name}
+
             engine = create_engine(url)
             connection = engine.connect()
             connection.close()
