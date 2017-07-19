3f8902d3fa4658611442a44bc96db3f9be70cdb4
“felipelm” <felipelm123@gmail.com>
2017-07-19 09:07:46 -0300

    build production

--- frontend/build/temp/production/Sky/sass/config.rb
+++ frontend/build/temp/production/Sky/sass/config.rb
@@ -1,6 +1,2 @@
 Compass.add_project_configuration('../../../../../sky/sass/config.rb')
-<<<<<<< HEAD
 cache_path = '/Users/felipemachado/Documents/linea/dri/frontend/build/.sass-cache'
-=======
-cache_path = '/home/fabio/Projetos/linea/dri/frontend/build/.sass-cache'
->>>>>>> develop
--- frontend/build/temp/production/Sky/sencha-compiler/app/full-page-master-bundle.js
+++ frontend/build/temp/production/Sky/sencha-compiler/app/full-page-master-bundle.js
@@ -1,6 +1,2 @@
 // @tag full-page
-<<<<<<< HEAD
 // @require /Users/felipemachado/Documents/linea/dri/frontend/sky/app.js
-=======
-// @require /home/fabio/Projetos/linea/dri/frontend/sky/app.js
->>>>>>> develop
