6c069bce1badd2850e2c7a2aaad0539ee1a6197c
lucasdpn <lucasdpnunes@gmail.com>
2017-10-31 13:43:16 -0200

    Permissions - user can only access public queries or created by him

--- api/userquery/views.py
+++ api/userquery/views.py
@@ -1,6 +1,10 @@
 import logging
 from .serializers import *
 from rest_framework import viewsets
+from rest_framework import permissions
+from rest_framework.authentication import TokenAuthentication, SessionAuthentication, BasicAuthentication
+from .permissions import IsOwnerOrPublic
+
 
 logger = logging.getLogger(__name__)
 
@@ -8,6 +12,9 @@ logger = logging.getLogger(__name__)
 class UserQueryViewSet(viewsets.ModelViewSet):
     queryset = Query.objects.all()
     serializer_class = UserQuerySerializer
+
+    authentication_classes = (TokenAuthentication, SessionAuthentication, BasicAuthentication)
+    permission_classes = (permissions.IsAuthenticated, IsOwnerOrPublic,)
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
