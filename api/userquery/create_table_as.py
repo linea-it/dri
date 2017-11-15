d9b9ed685ac26b99ef95f29ed309f47a791f8155
lucasdpn <lucasdpnunes@gmail.com>
2017-11-14 23:41:42 -0200

    refactoring

--- api/userquery/create_table_as.py
+++ api/userquery/create_table_as.py
@@ -15,69 +15,93 @@ from userquery.models import Job
 
 
 class CreateTableAs:
-    def __init__(self):
+    def __init__(self, job_id, user_id, table_name, schema=None, timeout=None):
+        self.table_name = table_name
+        self.schema = schema
+        self.timeout = timeout
+
+        self.user = User.objects.get(pk=user_id)
+        self.job = Job.objects.get(pk=job_id)
+
         # Get an instance of a logger
         self.logger = logging.getLogger('userquery create_table_as')
 
-    def create_table_by_job_id(self, job_id, user_id, table_name, schema=None, timeout=None):
-        self.logger.debug("CreateTableAs job_id: %s" % job_id)
+        # state variables
+        self.is_table_successfully_created = False
+        self.error_message = None
+
+    def do_all(self):
+        self._notify_by_email_start()
+        self._update_job_status_before_table_creation()
+        self._create_table_by_job_id()
+        self._update_job_status_after_table_creation_attempt()
+        self._send_notifications_by_email_after_table_creation_attempt()
+
+    def _update_job_status_before_table_creation(self):
+        self.job.job_status = 'rn'
+        self.job.save()
+
+    def _update_job_status_after_table_creation_attempt(self):
+        if self.is_table_successfully_created:
+            self.job.job_status = 'ok'
+        else:
+            self.job.job_status = 'er'
+
+        self.job.end_date_time = timezone.now()
+        self.job.save()
+
+    def _send_notifications_by_email_after_table_creation_attempt(self):
+        if self.is_table_successfully_created:
+            self._notify_by_email_finish()
+        else:
+            self._notify_user_by_email_failure(self.error_message)
+
+    def _create_table_by_job_id(self):
+        self.logger.debug("_create_table_by_job_id - job_id: %s" % self.job.pk)
 
         db = DBBase('catalog')
-        q = Job.objects.get(pk=job_id)
-        q.job_status = 'rn'
-
-        user = User.objects.get(pk=user_id)
         try:
-            self.notify_by_email_start(user, job_id, table_name)
-
-            # create table
-            db.create_table_raw_sql(table_name, q.sql_sentence, schema=schema, timeout=timeout)
-            q.job_status = 'ok'
-
-            self.notify_by_email_finish(user, q)
+            db.create_table_raw_sql(self.table_name, self.job.sql_sentence, schema=self.schema, timeout=self.timeout)
+            self.is_table_successfully_created = True
         except Exception as e:
-            self.logger.debug("CreateTableAs Error: %s" % str(e))
-            q.job_status = 'er'
+            self.error_message = str(e)
+            self.logger.debug("CreateTableAs Error: %s" % self.error_message)
 
-            self.notify_user_export_failure(user, table_name, str(e))
-        q.end_date_time = timezone.now()
-        q.save()
-
-    def notify_by_email_start(self, user, job_id, table_name):
-        if user.email:
+    def _notify_by_email_start(self):
+        if self.user.email:
             self.logger.info("Sending mail notification.")
             subject = "UserQuery - The creation of your table is being processed"
 
             body = render_to_string("job_notification_start.html", {
-                "username": user.username,
-                "id_job": job_id,
-                "table_name": table_name
+                "username": self.user.username,
+                "id_job": self.job.pk,
+                "table_name": self.table_name
             })
 
-            Notify().send_email(subject, body, user.email)
+            Notify().send_email(subject, body, self.user.email)
         else:
             self.logger.info("The user don't have a registered email")
 
-    def notify_by_email_finish(self, user, new_job):
-        if user.email:
+    def _notify_by_email_finish(self):
+        if self.user.email:
             self.logger.info("Sending mail notification.")
             host = settings.BASE_HOST
-            url = urljoin(host, os.path.join("userquery_job/%s/" % str(new_job.pk)))
+            url = urljoin(host, os.path.join("userquery_job/%s/" % str(self.job.pk)))
 
             subject = "The table creation is finished"
 
             body = render_to_string("job_notification_finish.html", {
-                "username": user.username,
+                "username": self.user.username,
                 "url": url,
-                "table_name": new_job.table_name
+                "table_name": self.job.table_name
             })
 
-            Notify().send_email(subject, body, user.email)
+            Notify().send_email(subject, body, self.user.email)
         else:
             self.logger.info("The user don't have a registered email")
 
-    def notify_user_export_failure(self, user, table_name, error_message):
-        if user.email:
+    def _notify_user_by_email_failure(self, error_message):
+        if self.user.email:
             self.logger.info("Sending mail notification FAILURE.")
 
             try:
@@ -87,12 +111,12 @@ class CreateTableAs:
 
             subject = "The table creation failed"
             body = render_to_string("job_notification_error.html", {
-                "username": user.username,
-                "table_name": table_name,
+                "username": self.user.username,
+                "table_name": self.table_name,
                 "error_message": error_message
             })
 
-            Notify().send_email(subject, body, user.email)
+            Notify().send_email(subject, body, self.user.email)
 
         else:
             self.logger.info("The user don't have a registered email")
--- api/userquery/tasks.py
+++ api/userquery/tasks.py
@@ -4,13 +4,10 @@ from celery import shared_task
 from .create_table_as import CreateTableAs
 
 
-create_table_as = CreateTableAs()
-
-
 @shared_task(name="create_table")
 def create_table(job_id, user_id, table_name, schema=None, timeout=None):
+    create_table_as = CreateTableAs(job_id, user_id, table_name, schema=schema, timeout=timeout)
     logger = create_table_as.logger
 
     logger.info("Task create_table_as has started")
-    create_table_as.create_table_by_job_id(job_id, user_id, table_name, schema=schema, timeout=timeout)
-
+    create_table_as.do_all()
