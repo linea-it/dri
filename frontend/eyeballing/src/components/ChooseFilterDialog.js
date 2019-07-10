a3c8a72ca481284c297207d3b0eeacca97dc6088
Matheus Allein <mtsallein@gmail.com>
2019-07-10 14:11:52 -0300

    Improvement of filter options

--- frontend/eyeballing/src/components/ChooseFilterDialog.js
+++ frontend/eyeballing/src/components/ChooseFilterDialog.js
@@ -62,7 +62,7 @@ function ChooseFilterDialog(props) {
                     <FormControlLabel
                         value={'null'}
                         control={<Radio />}
-                        label={'List tiles not inspected'}
+                        label={'List of tiles not inspected'}
                     />
                 </RadioGroup>
             </DialogContent>
