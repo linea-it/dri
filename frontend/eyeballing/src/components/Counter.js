2e6c3523a645e1bd29b6ec409dfc2be908aef105
Matheus Allein <mtsallein@gmail.com>
2019-09-30 11:48:15 -0300

    Adding a remote infinite scroll to the tile list and paginating the comments

--- frontend/eyeballing/src/components/Counter.js
+++ frontend/eyeballing/src/components/Counter.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import PropTypes from 'prop-types';
+
 const useStyles = makeStyles(theme => ({
   root: {
     // padding: theme.spacing(3, 2),
