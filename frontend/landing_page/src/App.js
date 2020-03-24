8f71f87783f0752468bd2d1ef2f3e1dfffcf4ccc
Glauber Costa Vila Verde <glauber.vila.verde@gmail.com>
2020-03-24 17:19:49 -0300

    Setup landing page in docker-compose and link with other apps

--- frontend/landing_page/src/App.js
+++ frontend/landing_page/src/App.js
@@ -4,8 +4,13 @@ function App() {
   return (
     <div className="App">
       <h1>DRI Home</h1>
+      <h1><a href={`${window.location.protocol}//${window.location.host}/sky/`}>Sky Viwer</a></h1>
+      <h1><a href={`${window.location.protocol}//${window.location.host}/target/`}>Target Viwer</a></h1>
+      <h1><a href={`${window.location.protocol}//${window.location.host}/userquery/`}>User Query</a></h1>
+      <h1><a href={`${window.location.protocol}//${window.location.host}/eyeballing/`}>Tile Inspection</a></h1>
     </div>
   );
 }
+
 
 export default App;
