de61e1742ab14d48789614f4b61af051309cb93c
Fernando <fernando_nicolau@outlook.com>
2020-04-20 15:58:23 -0300

    feat: page navigation routes not found. #1226

--- frontend/landing_page/src/Routes/index.js
+++ frontend/landing_page/src/Routes/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-
 import Header from '../components/Header';
 import Home from '../pages/Home';
 import AboutUs from '../pages/AboutUs';
 import Help from '../pages/Help';
 import Tutorials from '../pages/Tutorials';
 import Contact from '../pages/Contact';
+import Notfound from '../pages/NotFound';
 
 function Router() {
   return (
@@ -18,6 +18,7 @@ function Router() {
         <Route path="/help" component={Help} />
         <Route path="/tutorials" component={Tutorials} />
         <Route path="/contact-us" component={Contact} />
+        <Route component={Notfound} />
       </Switch>
     </BrowserRouter>
   );
