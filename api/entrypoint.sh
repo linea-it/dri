7b9cd7d0b85b84e64dda769c1ab26e02f3672e44
glaubervila <glauber.vila.verde@gmail.com>
2020-09-14 15:27:51 -0300

    UWSGI was added but settings are still missing.

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 python3 manage.py migrate
 
@@ -12,7 +13,9 @@ celery worker --workdir /app --app dri -l info &> /log/celery.log  &
 # Start Celery Beat
 celery worker --workdir /app --app dri -l info --beat &> /log/celery_beat.log  &
 
-python3 manage.py runserver 0.0.0.0:8000
+# python3 manage.py runserver 0.0.0.0:8000
 
-
+# Para producao usar uWSGI para servir o app e ter compatibilidade com Shibboleth
+# https://uwsgi-docs.readthedocs.io/en/latest/WSGIquickstart.html
+uwsgi --socket 0.0.0.0:8000 --wsgi-file /app/dri/wsgi.py --py-autoreload 1 --static-map /django_static/rest_framework/=/app/dri/static/rest_framework --static-map /django_static/admin/=/app/dri/static/admin
 
