abf94ed406e0c2e5e3d2aeb8a3966dcd65811e15
Fernando Silva <fernando_nicolau@outlook.com>
2020-09-17 12:00:53 -0300

    feat: getting interfaces through the api. #1226

--- frontend/landing_page/src/Services/api.js
+++ frontend/landing_page/src/Services/api.js
@@ -5,4 +5,16 @@ const api = axios.create({
     baseURL: 'https://desportal.cosmology.illinois.edu/dri/api/'
 });
 
+const baseUrl = 'https://desportal.cosmology.illinois.edu/dri/api/'
+
+export const getApplication = () =>
+  axios.all([
+    axios.get(`${baseUrl}application/?format=json&ordering=app_order`),
+  ])
+  .then(axios.spread((res) => res.data.filter(el => !el.app_disabled)))
+  .catch((err) => {
+    console.error(err);
+    return err;
+  });
+
 export default api;
--- frontend/landing_page/src/pages/Home/partials/Interfaces/index.js
+++ frontend/landing_page/src/pages/Home/partials/Interfaces/index.js
@@ -1,54 +1,58 @@
 /* eslint-disable max-len */
 /* eslint-disable no-unused-expressions */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
-// import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
-// import CardActions from '@material-ui/core/CardActions';
 import styles from './styles';
-// import DialogCard from '../ModalInterfaces';
+import { getApplication } from '../../../../Services/api';
 
 function Interfaces() {
   const classes = styles();
+  const [interfaces, setInterfaces] = useState([]);
+  // const interfaces = [
+  //   {
+  //     id: 1,
+  //     title: 'Sky Viewer',
+  //     // description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque dignissim consequat nibh, eu congue massa. Cras at placerat neque.',
+  //     pathname: '/sky/',
+  //     icon: 'sky_viewer.png',
+  //     image: 'dri1.jpg',
+  //   },
+  //   {
+  //     id: 2,
+  //     title: 'Target Viewer',
+  //     // description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque dignissim consequat nibh, eu congue massa. Cras at placerat neque.',
+  //     pathname: '/target/',
+  //     icon: 'target_viewer.png',
+  //     image: 'dri2.jpg',
+  //   },
+  //   {
+  //     id: 3,
+  //     title: 'Tile Inspection',
+  //     // description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque dignissim consequat nibh, eu congue massa. Cras at placerat neque.',
+  //     pathname: '/eyeballing/',
+  //     icon: 'sky_viewer.png',
+  //     image: 'dri4.jpg',
+  //   },
+  //   {
+  //     id: 4,
+  //     title: 'User Query',
+  //     // description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque dignissim consequat nibh, eu congue massa. Cras at placerat neque.',
+  //     pathname: '/userquery/',
+  //     icon: 'user_query.png',
+  //     image: 'dri3.jpg',
+  //   },
+  // ];
 
-  const interfaces = [
-    {
-      id: 1,
-      title: 'Sky Viewer',
-      // description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque dignissim consequat nibh, eu congue massa. Cras at placerat neque.',
-      pathname: '/sky/',
-      icon: 'sky_viewer.png',
-      image: 'dri1.jpg',
-    },
-    {
-      id: 2,
-      title: 'Target Viewer',
-      // description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque dignissim consequat nibh, eu congue massa. Cras at placerat neque.',
-      pathname: '/target/',
-      icon: 'target_viewer.png',
-      image: 'dri2.jpg',
-    },
-    {
-      id: 3,
-      title: 'Tile Inspection',
-      // description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque dignissim consequat nibh, eu congue massa. Cras at placerat neque.',
-      pathname: '/eyeballing/',
-      icon: 'sky_viewer.png',
-      image: 'dri4.jpg',
-    },
-    {
-      id: 4,
-      title: 'User Query',
-      // description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque dignissim consequat nibh, eu congue massa. Cras at placerat neque.',
-      pathname: '/userquery/',
-      icon: 'user_query.png',
-      image: 'dri3.jpg',
-    },
-  ];
+  useEffect(() => {
+    getApplication().then((res) => {
+      setInterfaces(res);
+    });
+  }, []);
 
   return (
     <div className={classes.root}>
@@ -60,18 +64,18 @@ function Interfaces() {
           justify="center"
           alignItems="stretch"
         >
-          {interfaces.map((item) => (
-            <Grid key={item.id} item xs={12} sm={6} md={3}>
+          {interfaces.map((item, index) => (
+            <Grid key={item.id} item xs={12} sm={6} md={4}>
               <Card>
                 <CardActionArea
-                  href={item.pathname}
+                  href={item.app_url}
                   target={item.url ? '_blanc' : '_self'}
                 >
                   <CardMedia
-                    alt={item.title}
+                    alt={item.app_display_name}
                     className={classes.media}
-                    image={`${process.env.PUBLIC_URL}/img/${item.image}`}
-                    title={item.title}
+                    image={`${process.env.PUBLIC_URL}/img/dri${index}.jpg`}
+                    title={item.app_display_name}
                   >
                     <Typography
                       gutterBottom
@@ -80,7 +84,7 @@ function Interfaces() {
                       component="h2"
                     >
                       {/* <i className={`fa fa-${item.icon}`}></i> &nbsp;  */}
-                      {item.title}
+                      {item.app_display_name}
                     </Typography>
                   </CardMedia>
                   {/* <CardContent className={classes.cardContent}>
