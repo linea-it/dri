dd2d84954f73aa8b1daaef2d93a2faaeca061799
Matheus Allein <mtsallein@gmail.com>
2020-09-18 16:34:29 -0300

    Fixing small bugs

--- frontend/landing_page/src/Services/api.js
+++ frontend/landing_page/src/Services/api.js
@@ -1,20 +1,11 @@
 import axios from 'axios';
 
-
-const api = axios.create({
-    baseURL: 'https://desportal.cosmology.illinois.edu/dri/api/'
-});
-
-const baseUrl = 'https://desportal.cosmology.illinois.edu/dri/api/'
-
 export const getApplication = () =>
   axios.all([
-    axios.get(`${baseUrl}application/?format=json&ordering=app_order`),
+    axios.get(`/application/?format=json&ordering=app_order`),
   ])
   .then(axios.spread((res) => res.data.filter(el => !el.app_disabled)))
   .catch((err) => {
     console.error(err);
     return err;
   });
-
-export default api;
