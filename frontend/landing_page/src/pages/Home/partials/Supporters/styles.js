f94c9d37c12bdafd658bcfea2e14b51012c7b6e3
Fernando Silva <fernando_nicolau@outlook.com>
2020-12-08 13:14:36 -0300

    feat: home page adjustments.

--- frontend/landing_page/src/pages/Home/partials/Supporters/styles.js
+++ frontend/landing_page/src/pages/Home/partials/Supporters/styles.js
@@ -3,7 +3,6 @@ import { makeStyles } from '@material-ui/core/styles';
 const styles = makeStyles((theme) => ({
   root: {
     marginTop: theme.spacing(6),
-    marginBottom: theme.spacing(12),
     width: '80%',
     margin: 'auto',
   },
