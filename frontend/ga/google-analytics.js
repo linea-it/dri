c2ef6c3688881479e7f054d0e1f5a4b2ff5613f6
Fernando Silva <fernando_nicolau@outlook.com>
2020-12-09 11:41:05 -0300

    feat: google analytics implementations. #1275

---
+++ frontend/ga/google-analytics.js
@@ -0,0 +1,23 @@
+// ## EXEMPLO DE GOOGLE ANALYTICS ##
+
+// COLABORATION
+// <!-- Global site tag (gtag.js) - Google Analytics -->
+// <script async src="https://www.googletagmanager.com/gtag/js?id=G-EEW8XVF479"></script>
+// <script>
+//   window.dataLayer = window.dataLayer || [];
+//   function gtag(){dataLayer.push(arguments);}
+//   gtag('js', new Date());
+
+//   gtag('config', 'G-EEW8XVF479');
+// </script>
+
+// PUBLIC
+// <!-- Global site tag (gtag.js) - Google Analytics -->
+// <script async src="https://www.googletagmanager.com/gtag/js?id=G-2JMWE98YJD"></script>
+// <script>
+//   window.dataLayer = window.dataLayer || [];
+//   function gtag(){dataLayer.push(arguments);}
+//   gtag('js', new Date());
+
+//   gtag('config', 'G-2JMWE98YJD');
+// </script>
