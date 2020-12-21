ab56151f3a1f52e2d2e7449baa486424ac81099a
glaubervila <glauber.vila.verde@gmail.com>
2020-12-21 16:48:35 -0300

    Closed #1275 The Google Analytics upload script has been changed.

--- frontend/ga/google-analytics.js
+++ frontend/ga/google-analytics.js
@@ -1,23 +1,26 @@
 // ## EXEMPLO DE GOOGLE ANALYTICS ##
+// To enable Google Analytics in one of the environments.
+// Create a new file called google-analytics.js copy and paste the template below, replace the ID G-XXXXXXXXXX with the Google Analitcs key.
+// In the compose docker mount the file in order to overwrite the template in the /var/www/ga directory.
 
-// COLABORATION
-// <!-- Global site tag (gtag.js) - Google Analytics -->
-// <script async src="https://www.googletagmanager.com/gtag/js?id=G-EEW8XVF479"></script>
-// <script>
-//   window.dataLayer = window.dataLayer || [];
-//   function gtag(){dataLayer.push(arguments);}
-//   gtag('js', new Date());
+// GA ID for Colaboration enviroment: G-EEW8XVF479
+// GA ID for Public enviroment: G-2JMWE98YJD
 
-//   gtag('config', 'G-EEW8XVF479');
-// </script>
+// Global site tag(gtag.js) - Google Analytics
+// NOTE: Uncomment the script below
 
-// PUBLIC
-// <!-- Global site tag (gtag.js) - Google Analytics -->
-// <script async src="https://www.googletagmanager.com/gtag/js?id=G-2JMWE98YJD"></script>
-// <script>
-//   window.dataLayer = window.dataLayer || [];
-//   function gtag(){dataLayer.push(arguments);}
-//   gtag('js', new Date());
+// function loadGoogleAnalytics() {
+//     var ga = document.createElement('script');
+//     ga.type = 'text/javascript';
+//     ga.async = true;
+//     ga.src = 'https://www.googletagmanager.com/gtag/js?id=G-EEW8XVF479';
+//     var s = document.getElementsByTagName('script')[0];
+//     s.parentNode.insertBefore(ga, s);
+// }
 
-//   gtag('config', 'G-2JMWE98YJD');
-// </script>
+// loadGoogleAnalytics(); 
+
+// window.dataLayer = window.dataLayer || [];
+// function gtag() { dataLayer.push(arguments); }
+// gtag('js', new Date());
+// gtag('config', 'G-EEW8XVF479');
