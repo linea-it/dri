87607767e692aafbbc5e33aee1f02411640c2464
glaubervila <glauber.vila.verde@gmail.com>
2021-04-08 20:20:51 -0300

    update wsgi and improve settings

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 python3 manage.py migrate
 
@@ -17,5 +16,14 @@ celery worker --workdir /app --app dri -l info --beat &> /log/celery_beat.log  &
 
 # Para producao usar uWSGI para servir o app e ter compatibilidade com Shibboleth
 # https://uwsgi-docs.readthedocs.io/en/latest/WSGIquickstart.html
-uwsgi --socket 0.0.0.0:8000 --wsgi-file /app/dri/wsgi.py --py-autoreload 1 --static-map /django_static/rest_framework/=/app/dri/static/rest_framework --static-map /django_static/admin/=/app/dri/static/admin
+uwsgi \
+  --socket 0.0.0.0:8000 \
+  --wsgi-file /app/dri/wsgi.py \
+  --py-autoreload 1 \
+  --static-map /django_static/rest_framework/=/app/dri/static/rest_framework \
+  --static-map /django_static/admin/=/app/dri/static/admin \
+  --buffer-size=32768
+  --processes=4 \
+  --threads=2 
+
 
