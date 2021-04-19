d7e85243744da8066678bb87717a5ff65bf4d905
Glauber Costa Vila-Verde <glauber.vila.verde@gmail.com>
2021-04-19 11:23:51 -0300

    Feat/shibboleth (#1346)
    * partial
    
    * partial
    
    * partial

--- api/dri/settings/defaults.py
+++ api/dri/settings/defaults.py
@@ -60,6 +60,7 @@ THIRD_PARTY_APPS = [
     'django_filters',
     'url_filter',
     'django_celery_results',
+    'shibboleth',
 ]
 
 PROJECT_APPS = [
@@ -86,6 +87,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'shibboleth.middleware.ShibbolethRemoteUserMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'current_user.CurrentUserMiddleware',
@@ -105,6 +107,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'django_settings_export.settings_export',
+                # 'shibboleth.context_processors.login_link',
             ],
         },
     },
@@ -197,7 +200,10 @@ CELERY = {
 
 # Lista as Variaveis que vao ser exportadas para os templates do Django. https://pypi.python.org/pypi/django-settings-export
 SETTINGS_EXPORT = [
-    'NCSA_SIGNUP_LINK'
+    'NCSA_SIGNUP_LINK',
+    'AUTH_SHIB_ENABLED',
+    'SHIB_LOGIN_GOOGLE_URL',
+    'SHIB_LOGIN_FACEBOOK_URL',
 ]
 
 # Variaveis a seguir Devem ser sobreescritas pelo local_vars.py
@@ -271,9 +277,29 @@ if AUTH_LDAP_ENABLED:
         AUTH_LDAP_USER_SEARCH_DN,
         ldap.SCOPE_SUBTREE, "(uid=%(user)s)"
     )
-# Including LDAP authentication:
-if AUTH_LDAP_ENABLED:
+
+    # Including LDAP authentication:
     AUTHENTICATION_BACKENDS += ('django_auth_ldap.backend.LDAPBackend',)
+
+# Shibboleth Authentication
+AUTH_SHIB_ENABLED = False
+SHIB_LOGIN_GOOGLE_URL = None
+SHIB_LOGIN_FACEBOOK_URL = None
+if AUTH_SHIB_ENABLED:
+
+    # https://github.com/Brown-University-Library/django-shibboleth-remoteuser
+    SHIBBOLETH_ATTRIBUTE_MAP = {
+        "eppn": (True, "username"),
+        "cn": (True, "first_name"),
+        "sn": (True, "last_name"),
+        "mail": (True, "email"),
+    }
+
+    # Including Shibboleth authentication:
+    AUTHENTICATION_BACKENDS += ('shibboleth.backends.ShibbolethRemoteUserBackend', )
+
+    SHIB_LOGIN_GOOGLE_URL = None
+    SHIB_LOGIN_FACEBOOK_URL = None
 
 # Email Notification configs
 # Dados de configuração do servidor de email que será usado para envio das notificações.
