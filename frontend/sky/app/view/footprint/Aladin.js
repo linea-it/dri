2ab941d8988ea886ce91328474871a973d78a1bb
Glauber Costa Vila-Verde <glauber.vila.verde@gmail.com>
2024-05-07 11:45:25 -0300

    Fixed #1466 - Map Viewer has been disabled (#1467)

--- frontend/sky/app/view/footprint/Aladin.js
+++ frontend/sky/app/view/footprint/Aladin.js
@@ -15,6 +15,8 @@ Ext.define('Sky.view.footprint.Aladin', {
             gotoSetPosition: false,
             enableShift: true,
             tilesGridVisible: false,
+            // Disabled Map Viewer Issue: #1466 - https://github.com/linea-it/dri/issues/1466
+            enableMaps: false, 
         });
 
         this.callParent(arguments);
