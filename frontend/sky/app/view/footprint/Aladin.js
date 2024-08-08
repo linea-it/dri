fd804a924825940a9618742a2f5aa30dd762a7a8
Glauber Costa Vila-Verde <glauber.vila.verde@gmail.com>
2024-08-08 15:52:23 -0300

    Enabled Maps in Skyviewer (#1545)
    * Enabled maps in skyviewer
    
    * Closed 1544 Enabled maps in skyviewer
    
    * Added initial data for dr2 maps

--- frontend/sky/app/view/footprint/Aladin.js
+++ frontend/sky/app/view/footprint/Aladin.js
@@ -15,8 +15,7 @@ Ext.define('Sky.view.footprint.Aladin', {
             gotoSetPosition: false,
             enableShift: true,
             tilesGridVisible: false,
-            // Disabled Map Viewer Issue: #1466 - https://github.com/linea-it/dri/issues/1466
-            enableMaps: false, 
+            enableMaps: true,
         });
 
         this.callParent(arguments);
