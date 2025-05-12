c6329c072514b529dfdd1aa4e75606a0050970ac
glaubervila <glauber.vila.verde@gmail.com>
2025-05-12 17:11:07 -0300

    Changed django_static folder

--- api/dri/settings/defaults.py
+++ api/dri/settings/defaults.py
@@ -156,7 +156,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "django_static")
 STATIC_URL = "/django_static/"
 
 AUTHENTICATION_BACKENDS = (
