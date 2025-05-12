da577bca983d5d4a3937215a97f377311c035e47
Glauber Costa Vila-Verde <glauber.vila.verde@gmail.com>
2025-05-12 17:11:43 -0300

    Add permissions (#1606)
    * Added write permission to django_static folder
    
    * Changed django_static folder

--- api/start.sh
+++ api/start.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
+
+echo "Running Migrate to apply changes in Admin database"
 python3 manage.py migrate
 
+echo "Running Migrate to apply changes in Catalog database"
 python3 manage.py migrate catalog --database=catalog
 
+echo "Running Collect Statics"
 python manage.py collectstatic --clear --noinput --verbosity 0
 
 
-# # Start Celery Workers
-# celery worker --workdir /app --app dri -l info &> /log/celery.log  &
-
-# # Start Celery Beat
-# celery worker --workdir /app --app dri -l info --beat &> /log/celery_beat.log  &
-
-# python3 manage.py runserver 0.0.0.0:8000
-
+echo "Running Django with uWSGI"
 # Para producao usar uWSGI para servir o app e ter compatibilidade com Shibboleth
 # https://uwsgi-docs.readthedocs.io/en/latest/WSGIquickstart.html
 uwsgi \
@@ -21,8 +18,8 @@ uwsgi \
   --http-socket 0.0.0.0:8001 \
   --wsgi-file /app/dri/wsgi.py \
   --py-autoreload 1 \
-  --static-map /django_static/rest_framework/=/app/dri/static/rest_framework \
-  --static-map /django_static/admin/=/app/dri/static/admin \
+  --static-map /django_static/rest_framework/=/app/dri/django_static/rest_framework \
+  --static-map /django_static/admin/=/app/dri/django_static/admin \
   --buffer-size=32768 \
   --processes=4 \
   --threads=2 
